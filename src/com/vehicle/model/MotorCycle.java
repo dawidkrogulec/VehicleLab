e1d613052c31e729f75814a7e5351f07bd5d818e
dawidkrogulec <dawid.krogulec@gmail.com>
2022-03-18 12:47:29 +0100

    Finish MotorCycle Class

--- src/com/vehicle/model/MotorCycle.java
+++ src/com/vehicle/model/MotorCycle.java
@@ -1,12 +1,35 @@
 package com.vehicle.model;
 
 public class MotorCycle extends VehicleAbstract{
-    //dodaj jakieś pola specyficzne dla motoru
-    //Nowy komentarz
 
+    private final Integer maxPassenger;
+    private Boolean isCross;
+
+    public MotorCycle(String make, String model, Integer yearOfManufacture, Integer numberOfWheels, Double engineCapacity, Double price, Integer maxPassenger, Boolean isCross) {
+        super(make, model, yearOfManufacture, numberOfWheels, engineCapacity, price);
+        this.maxPassenger = maxPassenger;
+        this.isCross = isCross;
+    }
+
+    public MotorCycle(String make, String model, Integer yearOfManufacture, Integer numberOfWheels, Double engineCapacity, Double price, Integer maxPassenger) {
+        super(make, model, yearOfManufacture, numberOfWheels, engineCapacity, price);
+        this.maxPassenger = maxPassenger;
+    }
 
     @Override
     public String makeNoise() {
         return "Whoom Whoom";
+    }
+
+    public Integer getMaxPassenger() {
+        return maxPassenger;
+    }
+
+    public Boolean getCross() {
+        return isCross;
+    }
+
+    public void setCross(Boolean cross) {
+        isCross = cross;
     }
 }
